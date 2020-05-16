db1118422042b7d6178f62a65256732195ae579d
enofeb <eneszor95@gmail.com>
2020-05-16 15:42:41 +0300

    Constraint layout was imported and base activity default layout was declared

--- base/core/src/main/java/com/example/core/base/BaseActivity.kt
+++ base/core/src/main/java/com/example/core/base/BaseActivity.kt
@@ -1,2 +1,17 @@
 package com.example.core.base
 
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import com.example.core.R
+
+open class BaseActivity : AppCompatActivity() {
+
+    @LayoutRes
+    open val layoutRes = R.layout.activity_container
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layoutRes)
+    }
+}
