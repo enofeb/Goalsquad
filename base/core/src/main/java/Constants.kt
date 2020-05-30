bad8a4e941b0373bdedc5d06455f488458df4d49
enofeb <eneszor95@gmail.com>
2020-05-30 14:56:50 +0300

    Base fragment class was updated

---
+++ base/core/src/main/java/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.core
+
+class Constants private constructor() {
+    companion object {
+        const val NO_RES = 0
+    }
+}
--- base/core/src/main/java/com/example/core/base/BaseFragment.kt
+++ base/core/src/main/java/com/example/core/base/BaseFragment.kt
@@ -1,5 +1,29 @@
 package com.example.core.base
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import com.example.core.Constants
 
-open class BaseFragment : Fragment()
+abstract class BaseFragment : Fragment() {
+
+    open val titleRes = Constants.NO_RES
+
+    @LayoutRes
+    protected abstract fun getLayoutRes(): Int
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        if (getLayoutRes() != Constants.NO_RES) {
+            return inflater.inflate(getLayoutRes(), null, false)
+        }
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
