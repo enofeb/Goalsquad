71b05bd0d5e464b8a7edc29bf6d6af3923ff99a9
enofeb <eneszor95@gmail.com>
2020-05-30 15:21:05 +0300

    Dashboard activity was added

---
+++ feature/dashboard/src/main/java/com/example/dashboard/presentation/dashboard/DashboardActivity.kt
@@ -0,0 +1,10 @@
+package com.example.dashboard.presentation.dashboard
+
+import com.example.core.base.BaseActivity
+import com.example.dashboard.R
+
+class DashboardActivity : BaseActivity() {
+
+    override val layoutRes = R.layout.activity_dashboard
+
+}
