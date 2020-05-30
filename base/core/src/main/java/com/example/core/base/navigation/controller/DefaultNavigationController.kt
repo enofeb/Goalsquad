5ee66317679d2b0e8271303f2587609b4d3bad6f
enofeb <eneszor95@gmail.com>
2020-05-30 21:36:21 +0300

    Default navigation controller class was added

--- base/core/src/main/java/com/example/core/base/navigation/controller/DefaultNavigationController.kt
+++ base/core/src/main/java/com/example/core/base/navigation/controller/DefaultNavigationController.kt
@@ -3,8 +3,5 @@ package com.example.core.base.navigation.controller
 import androidx.fragment.app.FragmentActivity
 import java.lang.ref.WeakReference
 
-interface NavigationController {
-
-    val activity: WeakReference<FragmentActivity>
-
-}
+class DefaultNavigationController(override val activity: WeakReference<FragmentActivity>) :
+    NavigationController
