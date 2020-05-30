357597f9dfe1aa9dc37d31026f7fd1aa0e375a2a
enofeb <eneszor95@gmail.com>
2020-05-30 16:16:50 +0300

    Bottom navigation was set it up with dashboard activity

--- app/src/main/java/com/example/goalsquad/HomeActivity.kt
+++ app/src/main/java/com/example/goalsquad/HomeActivity.kt
@@ -1,12 +1,17 @@
 package com.example.goalsquad
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.dashboard.presentation.dashboard.DashboardActivity
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val i = Intent(this, DashboardActivity::class.java)
+        startActivity(i)
     }
 }
