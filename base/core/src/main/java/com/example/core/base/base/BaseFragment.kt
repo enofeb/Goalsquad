c9b78ffa4b094351dea700539cb2c016d7b42b3f
enofeb <eneszor95@gmail.com>
2020-06-27 13:06:57 +0300

    Navigation controller object was added in base fragment

--- base/core/src/main/java/com/example/core/base/base/BaseFragment.kt
+++ base/core/src/main/java/com/example/core/base/base/BaseFragment.kt
@@ -8,6 +8,9 @@ import androidx.annotation.LayoutRes
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import com.example.core.base.Constants
+import com.example.core.base.navigation.controller.DefaultNavigationController
+import com.example.core.base.navigation.controller.NavigationController
+import java.lang.ref.WeakReference
 
 abstract class BaseFragment : Fragment() {
 
@@ -16,6 +19,15 @@ abstract class BaseFragment : Fragment() {
 
     @LayoutRes
     protected abstract fun getLayoutRes(): Int
+
+
+    protected lateinit var navigationController: NavigationController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        navigationController = DefaultNavigationController(WeakReference(activity!!))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
