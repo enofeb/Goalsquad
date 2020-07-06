7eee2cf3c0c21bc7204044656e0e06cb399b5afe
enofeb <eneszor95@gmail.com>
2020-07-06 22:46:06 +0300

    Navigator class was updated

--- base/core/src/main/java/com/example/core/base/navigation/Navigator.kt
+++ base/core/src/main/java/com/example/core/base/navigation/Navigator.kt
@@ -3,12 +3,26 @@ package com.example.core.base.navigation
 import android.content.Intent
 
 const val PACKAGE_NAME = "com.example.goalsquad"
+private val classes = mutableMapOf<String, Class<*>>()
+
+private inline fun <reified T : Any> Any.cast() = this as? T
 
 private fun intentTo(className: String) =
     Intent(Intent.ACTION_VIEW).setClassName(PACKAGE_NAME, className)
+
+private fun <T> String.getFeature(): Class<out T>? =
+    classes.getOrPut(this) {
+        try {
+            Class.forName(this)
+        } catch (e: ClassNotFoundException) {
+            return null
+        }
+    }.cast()
+
 
 fun String.createIntentFeature() = try {
     Class.forName(this).run { intentTo(this@createIntentFeature) }
 } catch (e: ClassNotFoundException) {
     null
 }
+
