3749b3e755e55045d9bfbfd1bfc0c0b3f9bddc6f
enofeb <eneszor95@gmail.com>
2020-07-11 15:01:01 +0300

    Bottom menu titles were updated

--- feature/matches/src/main/java/com/example/matches/presentation/MatchesHomeFragment.kt
+++ feature/matches/src/main/java/com/example/matches/presentation/MatchesHomeFragment.kt
@@ -1,2 +1,12 @@
 package com.example.matches.presentation
 
+import com.example.core.com.example.core.base.base.BaseFragment
+import com.example.matches.R
+
+class MatchesHomeFragment : BaseFragment() {
+    override fun getLayoutRes() = R.layout.fragment_matches_home
+
+    companion object {
+        fun newInstance() = MatchesHomeFragment()
+    }
+}
