4e7e0734db2ba1257ef7941cabbdf3f48cd768ef
enofeb <eneszor95@gmail.com>
2020-07-28 22:33:23 +0300

    Okhttp method was added in api module

--- base/core/src/main/java/com/example/core/base/di/ApiModule.kt
+++ base/core/src/main/java/com/example/core/base/di/ApiModule.kt
@@ -1,11 +1,24 @@
 package com.example.core.base.di
 
 import com.example.core.BuildConfig
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
 
 
-private fun provideApiUrl()=BuildConfig.BASE_URL
+private fun provideApiUrl() = BuildConfig.BASE_URL
 
-fun provideLoggingInterceptor():HttpLoggingInterceptor=
-    HttpLoggingInterceptor().apply { level=HttpLoggingInterceptor.Level.BODY }
+fun provideLoggingInterceptor(): HttpLoggingInterceptor =
+    HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+
+fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+    val okHttpClientBuilder = OkHttpClient.Builder().apply {
+        // addInterceptor(KeyInter) ADD INTERCEPTOR CLASS AND GO ON
+        addInterceptor(loggingInterceptor)
+        connectTimeout(30, TimeUnit.SECONDS)
+        readTimeout(30, TimeUnit.SECONDS)
+    }
+    return okHttpClientBuilder.build()
+}
 
