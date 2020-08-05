ab3b1738fe3603ca68ba30b1d613f656ae5d3013
enofeb <eneszor95@gmail.com>
2020-08-05 22:56:57 +0300

    Api module was completed

--- base/core/src/main/java/com/example/core/base/di/ApiModule.kt
+++ base/core/src/main/java/com/example/core/base/di/ApiModule.kt
@@ -1,11 +1,45 @@
 package com.example.core.base.di
 
 import com.example.core.BuildConfig
-import okhttp3.OkHttp
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
+private const val GOAL_SQUAD_URL = "goal_squad_url"
+private const val GOAL_SQUAD_CLIENT = "goal_squad_client"
+private const val GOAL_SQUAD_RETROFIT = "goal_squad_client"
+
+val apiModule = module {
+    factory(named(GOAL_SQUAD_URL)) {
+        provideApiUrl()
+    }
+
+    single {
+        provideLoggingInterceptor()
+    }
+
+    single(named(GOAL_SQUAD_CLIENT)) {
+        provideOkHttpClient(loggingInterceptor = get())
+    }
+
+    single {
+        provideGson()
+    }
+
+    single(named(GOAL_SQUAD_RETROFIT)) {
+        provideRetrofit(
+            baseUrl = get(named(GOAL_SQUAD_URL)),
+            client = get(named(GOAL_SQUAD_CLIENT)),
+            gson = get()
+        )
+    }
+}
 
 private fun provideApiUrl() = BuildConfig.BASE_URL
 
@@ -14,11 +48,24 @@ fun provideLoggingInterceptor(): HttpLoggingInterceptor =
 
 fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
     val okHttpClientBuilder = OkHttpClient.Builder().apply {
-        // addInterceptor(KeyInter) ADD INTERCEPTOR CLASS AND GO ON
+        // addInterceptor(KeyInter)
         addInterceptor(loggingInterceptor)
         connectTimeout(30, TimeUnit.SECONDS)
         readTimeout(30, TimeUnit.SECONDS)
     }
     return okHttpClientBuilder.build()
 }
+
+fun provideGson(): Gson {
+    val gsonBuilder = GsonBuilder()
+    return gsonBuilder.create()
+}
+
+fun provideRetrofit(baseUrl: String, client: OkHttpClient, gson: Gson): Retrofit =
+    with(Retrofit.Builder()) {
+        baseUrl(baseUrl)
+        client(client)
+        addConverterFactory(GsonConverterFactory.create(gson))
+        build()
+    }
 
