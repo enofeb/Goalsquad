f4d88f0d677738e382b5b6fba63ea5e80caddf7d
enofeb <eneszor95@gmail.com>
2020-09-27 22:23:13 +0300

    Unused import codes were removed

--- feature/matches/src/main/java/com/example/matches/presentation/MatchesHomeFragment.kt
+++ feature/matches/src/main/java/com/example/matches/presentation/MatchesHomeFragment.kt
@@ -1,13 +1,10 @@
 package com.example.matches.presentation
 
-import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
 import com.example.core.base.model.DataHolder
-import com.example.core.base.repository.matches.MatchesRepository
 import com.example.core.com.example.core.base.base.BaseFragment
 import com.example.matches.R
-import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MatchesHomeFragment : BaseFragment() {
