aa5f99e956315fbc84626993da272515ff2bea40
enofeb <eneszor95@gmail.com>
2020-09-27 23:20:40 +0300

    Match list adapter was updated

--- feature/matches/src/main/java/com/example/matches/presentation/matches/MatchListAdapter.kt
+++ feature/matches/src/main/java/com/example/matches/presentation/matches/MatchListAdapter.kt
@@ -1,25 +1,30 @@
 package com.example.matches.presentation.matches
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.core.base.model.matches.Match
+import com.example.matches.R
 
 class MatchListAdapter : RecyclerView.Adapter<MatchListAdapter.MatchListViewHolder>() {
 
+    private var items: MutableList<Match?> = mutableListOf()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchListViewHolder {
-        TODO("Not yet implemented")
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_match_list, parent, false)
+        return MatchListViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = items.size
 
     override fun onBindViewHolder(holder: MatchListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        //TODO MOVE ON FROM HERE
+        //TODO HAVE A LOOK AT DIFF UTIL AND DELEGATION
     }
 
     class MatchListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {}
-
 
 
 }
